96acff94b4f94e401b0663c9ae4c98e00ad6650d
Jackson085 <jackson26594916@gmail.com>
2025-06-14 11:25:11 +0200

    add XML documentation for location retrieval and user management methods in LocationController and UserController

--- Controllers/LocationController.cs
+++ Controllers/LocationController.cs
@@ -15,9 +15,15 @@ namespace FriendsAndPlaces.Controllers
         }
 
         //service3
-        //Request Ready
-        //Implementation Ready
-        // (username: friendsandplaces , password: Dvelop1!)
+        /// <summary>
+        /// Retrieves the city name for a given postal code.
+        /// </summary>
+        /// <param name="postalcode">The postal code to look up.</param>
+        /// <param name="username">The username for authentication (friendsandplaces).</param>
+        /// <returns>Returns the city name associated with the postal code.</returns>
+        /// <response code="200">Returns the city name.</response>
+        /// <response code="400">If the postal code is invalid or not found.</response>
+        /// <response code="401">If the username is incorrect.</response>
         [HttpGet("getOrt")]
         public async Task<IActionResult> GetLocationFromPostalCode([FromQuery] string postalcode, [FromQuery] string username)
         {
@@ -26,7 +32,16 @@ namespace FriendsAndPlaces.Controllers
         }
 
         //service9
-        //Request Ready
+        /// <summary>
+        /// Retrieves location information based on the provided address details.
+        /// </summary>
+        /// <param name="land">The country.</param>
+        /// <param name="plz">The postal code.</param>
+        /// <param name="ort">The city.</param>
+        /// <param name="strasse">The street address.</param>
+        /// <returns>Returns Ok if the location is found.</returns>
+        /// <response code="200">Location found successfully.</response>
+        /// <response code="400">If the address is invalid or not found.</response>
         [HttpGet("getStandortPerAdresse")]
         public async Task<IActionResult> GetLocationFromAddress([FromQuery] string land, [FromQuery] string plz, [FromQuery] string ort, [FromQuery] string strasse)
         {
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,7 +1,6 @@
 ﻿using FriendsAndPlaces.Models.API;
 using FriendsAndPlaces.Services;
-using Microsoft.AspNetCore.Mvc;
-using System.Reflection;
+using Microsoft.AspNetCore.Mvc; 
 
 namespace FriendsAndPlaces.Controllers
 {
@@ -17,8 +16,14 @@ namespace FriendsAndPlaces.Controllers
         }
 
         //service8
-        //Request Ready
-        //Implementation Ready
+        /// <summary>
+        /// Retrieves all users.
+        /// </summary>
+        /// <param name="login">The login name for authentication.</param>
+        /// <param name="session">The session token for authentication.</param>
+        /// <returns>Returns a list of all users if the authentication is valid.</returns>
+        /// <response code="200">Returns the list of users.</response>
+        /// <response code="400">If the authentication is invalid.</response>
         [HttpGet("getBenutzer")]
         public async Task<IActionResult> GetUser([FromQuery] string login, [FromQuery] string session)
         {
@@ -33,7 +38,15 @@ namespace FriendsAndPlaces.Controllers
         }
 
         //service7
-        //Request Ready
+        /// <summary>
+        /// Retrieves the location of a user.
+        /// </summary>
+        /// <param name="login">The login name for authentication.</param>
+        /// <param name="session">The session token for authentication.</param>
+        /// <param name="id">The ID of the user.</param>
+        /// <returns>Returns the coordinates of the user if online and authenticated.  If the user is offline, it should return the home address. Currently, it returns Ok().</returns>
+        /// <response code="200">Returns the coordinates of the user.</response>
+        /// <response code="400">If the authentication is invalid.</response>
         [HttpGet("getStandort")]
         public async Task<IActionResult> GetLocation([FromQuery] string login, [FromQuery] string session, [FromQuery] string id)
         {
@@ -58,8 +71,13 @@ namespace FriendsAndPlaces.Controllers
         }
 
         //service6
-        //Request Ready
-        //Implementation Ready
+        /// <summary>
+        /// Sets the location coordinates for a user.
+        /// </summary>
+        /// <param name="model">The location data containing the login name, session token, and coordinates.</param>
+        /// <returns>Returns Ok if the location is successfully updated.</returns>
+        /// <response code="200">Location updated successfully.</response>
+        /// <response code="400">If the authentication is invalid.</response>
         [HttpPut("setStandort")]
         public async Task<IActionResult> SetLocation([FromBody] LocationModel model)
         {
